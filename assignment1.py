909932b8f22c339a7464c6c729d200ee5c4bfac8
Amatullah Mir <amatullahmir@Amatullahs-MacBook-Pro.local>
2020-10-08 19:59:35 -0500

    first assignment

---
+++ assignment1.py
@@ -0,0 +1,61 @@
+def add_one_and_multiply(a, x):
+    """ Add 1 to a, and multiply by x"""
+
+    ### EXERCISE 1 -- YOUR CODE GOES HERE
+    # Replace "None" with the correct expression
+    r = (1+a)*x
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return r
+
+
+def within_range(x, lb, ub):
+    """ Is x strictly between lb and ub?"""
+
+    ### EXERCISE 2 -- YOUR CODE GOES HERE
+    # Replace "None" with the correct expression
+    r = lb < x < ub
+    bool(r)
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return r
+
+
+def number_string(x):
+    if x > 0:
+        s = 'POSITIVE'
+    elif x < 0:
+        s = 'NEGATIVE'
+    else:
+        s = 'ZERO'
+    return s
+
+
+def num_divisible(lb, ub, p, q):
+    n = 0
+    for i in range(lb, ub):
+        if (i%p==0) and (i%q==0):
+            n = n + 1
+        else:
+            n = 0
+    return n
+
+
+def count_negative(lst):
+    n = 0
+    for num in lst:
+        if x < 0:
+            n += x
+        else:
+            n = 0
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return n
+
+
+def negate_list(lst):
+    new_lst = []
+    for i in new_lst:
+        new_lst.append(i * -1)
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return new_lst
